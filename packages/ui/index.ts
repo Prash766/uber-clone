f33165bdf9c82e4caa6b8ebc5f7a898a60429989
Prash766 <prashantshukla766@gmail.com>
2025-02-02 18:30:23 +0530

    feat(ui): add Label component and update dependencies

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,6 +1,6 @@
-export { default as NavBar } from './src/NavBar';
 export * from './src/components/ui/card'
 export * from './src/components/ui/input'
 export * from './src/components/ui/button'
+export * from './src/components/ui/label'
 
  
