fc9daafb004040db846f0b73e3d34792597edcd7
Prash766 <prashantshukla766@gmail.com>
2025-02-28 11:53:13 +0530

    feat(server): refactor server setup to use Socket.IO and remove deprecated app file

---
+++ apps/server/src/utils/socket.ts
@@ -0,0 +1,42 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+import userRouter from "../routes/user.routes";
+import captainRouter from "../routes/captain.routes";
+import locationRouter from "../routes/location.routes";
+import rideRouter from "../routes/ride.routes";
+import errorMiddleware from "../middleware/error.middleware";
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/captain", captainRouter);
+app.use("/api/v1/location", locationRouter);
+app.use("/api/v1/ride", rideRouter);
+
+app.use(errorMiddleware);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [process.env.FRONTEND_URL as string],
+    credentials: true,
+  },
+});
+
+export { io, server, app };
